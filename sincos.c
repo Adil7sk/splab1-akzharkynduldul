6c64f9fbbb85f57fc596b14848e67f76c3e3ce81
akzharkynduldul <akzharkyn.duldul@ce.sdu.edu.kz>
2017-09-25 21:49:52 +0600

    TaskA sincos

--- sincos.c
+++ sincos.c
@@ -1,9 +1,34 @@
-//Fullname: Vasya Pupkin
-//Group: 3EN04X
-
+//Fullname : Akzharkyn Duldul
+//Nickname: White 
+//Group : 3EN04B
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+ #include <string.h>
 
 int main(int argc, char*argv[]){
-  return 0;
+    if(argc == 2){
+        char * n = argv[1];
+        double number = atof(n);
+        printf("%lf \n" , sin(number));
+        printf("%lf" , cos(number));    
+    }
+    if(argc == 3){
+        char *n = argv[1];
+        if(strcmp("-s", n) == 0){
+            char * num = argv[2];
+            double number = atof(num);
+            printf("%lf \n", sin(number));        
+        }    
+        if(strcmp("-c", n) == 0){
+            char * num = argv[2];
+            double number = atof(num);
+            printf("%lf \n", cos(number));        
+        }    
+    }
+    if(argc == 1){
+        char *instruction = "usage: sincos angle \n only sine: sincos -s angle \n only cosine: sincos -c angle \n";
+        printf("%s", instruction);    
+    }
+    return 0;
 }
